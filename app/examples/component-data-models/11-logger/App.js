3bbeb66c2a9b76eae9a1a50251b5addc38f5a6bf
Grgur Grisogono <grgur@grgur.com>
2015-10-03 19:54:56 +0200

    Restart on success

--- app/examples/component-data-models/11-logger/App.js
+++ app/examples/component-data-models/11-logger/App.js
@@ -14,18 +14,25 @@ export default class App extends Component {
     this.setState({success: true});
   }
 
+  restart() {
+    this.setState({
+      game: new Game(),
+      success: false
+    });
+  }
+
   render() {
     const { game, success } = this.state;
     const successCls = success ? 'win' : 'gameon';
 
     return (
       <div className="game">
-        <div className={successCls}>Game over!</div>
+        <div className={successCls} onClick={::this.restart}>Game over!</div>
         <div className="board">
             <Board game={game} />
         </div>
         <div className="results">
-          <Results game={game} onSuccess={::this.onSuccess}/>
+          <Results game={game} onSuccess={::this.onSuccess} />
         </div>
       </div>
     );
